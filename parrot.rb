d9921f7a66c825de0e2b2546d923e7ceef0e8363
donellgill <donell.gill@outlook.com>
2021-06-28 17:51:00 -0400

    done

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(name, phrase = "Squawk")
+    puts("Hi I'm #{name}. I like to #{phrase}.")
+    return parrot
+end
+
+parrot("Jerry", "play ball")
